952b84ec6777563a51424a9eed24ca32a5204ccd
fujieri <jinnatengben@gmail.com>
2023-09-16 21:25:44 +0900

    閏年アルゴリズムの作成

--- leap_year_algorithm/leap_year_algorithm.dart
+++ leap_year_algorithm/leap_year_algorithm.dart
@@ -1,3 +1,15 @@
+import 'dart:io';
 void main(List<String> arguments) {
-  print('Hello world!');
+  print('整数を入力してください。');
+  String input = stdin.readLineSync() ?? ''; // 標準入力から文字列を読み込む
+  int number = int.parse(input); // 文字列を整数に変換
+  //var isLeapYear = number;
+  if (number % 4 == 0 && !(number % 100 == 0 && number % 400 != 0)){
+    print('閏年です。');
+  }else{
+    print('閏年ではない。');
+  }
 }
+
+
+
