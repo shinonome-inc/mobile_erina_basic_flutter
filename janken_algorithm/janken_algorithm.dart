7f1adcaf7f1c3963cdd5dec02219c24608c166f0
fujieri <jinnatengben@gmail.com>
2023-10-20 21:16:43 +0900

    じゃんけんアルゴリズム作成

--- janken_algorithm/janken_algorithm.dart
+++ janken_algorithm/janken_algorithm.dart
@@ -1,3 +1,23 @@
+import 'dart:io';
+import 'dart:math' as math;
+
 void main(List<String> arguments) {
-  print('Hello world!');
+  int yh = math.Random().nextInt(3);
+
+  print("じゃんけんの手を入力してください（グーなら１、チョキなら２、パーなら３のキーボード)");
+  String input = stdin.readLineSync() ?? '';
+  int mh = int.parse(input);
+
+  if(mh == yh) {
+    print("あいこ");
+  }else if((mh == 1&& yh == 2)||(mh == 2 && yh == 3)||(mh == 3 && yh == 1)) {
+    print("勝ち");
+  }else {
+    print("負け");
+  }
 }
+
+
+
+
+
