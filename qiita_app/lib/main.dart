2d5a348069fa54b763b7c4187852bf70d97659a0
fujieri <jinnatengben@gmail.com>
2024-03-04 16:45:50 +0900

    add toppage

--- qiita_app/lib/main.dart
+++ qiita_app/lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:qiita_app/constants/app_colors.dart';
+import 'package:qiita_app/pages/top_pages.dart'; // top_pages.dart ファイルをインポート
 
 void main() {
   runApp(const MyApp());
@@ -8,96 +8,10 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        body: Stack(
-          children: [
-            Container(
-              decoration: const BoxDecoration(
-                image: DecorationImage(
-                  image: AssetImage('images/background.png'),
-                  fit: BoxFit.cover,
-                ),
-              ),
-            ),
-            Container(
-              width: double.infinity,
-              height: double.infinity,
-              color: Colors.black.withOpacity(0.2),
-            ),
-            Container(
-              padding: const EdgeInsets.all(24.0),
-              width: double.infinity,
-              height: double.infinity,
-              child: Column(
-                children: [
-                  const SizedBox(
-                    height: 220,
-                  ),
-                  const Text(
-                    'Qiita Feed App',
-                    style: TextStyle(
-                      fontSize: 36,
-                      color: Colors.white,
-                      fontFamily: 'Pacifico',
-                    ),
-                  ),
-                  const SizedBox(),
-                  const Text(
-                    '-PlayGround-',
-                    style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      fontSize: 14,
-                      color: Colors.white,
-                    ),
-                  ),
-                  const Spacer(),
-                  SizedBox(
-                    height: 50,
-                    width: double.infinity,
-                    child: ElevatedButton(
-                      onPressed: () {},
-                      style: ElevatedButton.styleFrom(
-                        backgroundColor: AppColors.green,
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(25),
-                        ),
-                      ),
-                      child: const Text(
-                        'ログイン',
-                        style: TextStyle(fontWeight: FontWeight.bold),
-                      ),
-                    ),
-                  ),
-                  const SizedBox(
-                    height: 16,
-                  ),
-                  SizedBox(
-                    height: 50,
-                    child: TextButton(
-                      onPressed: () {},
-                      child: const Text(
-                        'ログインせずに利用する',
-                        style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                          fontSize: 15,
-                          color: Colors.white,
-                        ),
-                      ),
-                    ),
-                  ),
-                  const SizedBox(
-                    height: 64,
-                  ),
-                ],
-              ),
-            ),
-          ],
-        ),
-      ),
+    return const MaterialApp(
+      home: TopPage(), // TopPage を表示
     );
   }
 }
