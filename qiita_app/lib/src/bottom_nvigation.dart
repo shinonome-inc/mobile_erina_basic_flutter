62a48b29875d0bed2930a1b058b63804e70606bb
fujieri <jinnatengben@gmail.com>
2024-09-06 16:44:00 +0900

    記事ページ

--- qiita_app/lib/src/bottom_nvigation.dart
+++ qiita_app/lib/src/bottom_nvigation.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:qiita_app/src/constants/app_colors.dart';
-import 'package:qiita_app/src/pages/feed.dart';
+import 'package:qiita_app/src/pages/feedpage/feed.dart';
 import 'package:qiita_app/src/pages/mypage.dart';
 import 'package:qiita_app/src/pages/setting.dart';
 import 'package:qiita_app/src/pages/tag.dart';
@@ -16,7 +16,7 @@ class BottomNavigation extends StatefulWidget {
 class BottomNavigationState extends State<BottomNavigation> {
   int selectedIndex = 0;
   final List<Widget> pageList = [
-    const FeedPage(),
+    const Feed(),
     const Tag(),
     const MyPage(),
     const Setting()
